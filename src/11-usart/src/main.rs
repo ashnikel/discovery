aa0ea0e5119499c49755919b584de2be4d24e9c7
ashnikel <ashnikel@gmail.com>
2020-04-05 22:58:43 +0300

    Reverse a string

--- src/11-usart/src/main.rs
+++ src/11-usart/src/main.rs
@@ -2,59 +2,48 @@
 #![no_main]
 #![no_std]
 
-use core::fmt::{self, Write};
-
 #[allow(unused_imports)]
-use aux11::{entry, iprint, iprintln, usart1};
-
-macro_rules! uprint {
-    ($serial:expr, $($arg:tt)*) => {
-        $serial.write_fmt(format_args!($($arg)*)).ok()
-    };
-}
-
-macro_rules! uprintln {
-    ($serial:expr, $fmt:expr) => {
-        uprint!($serial, concat!($fmt, "\n"))
-    };
-    ($serial:expr, $fmt:expr, $($arg:tt)*) => {
-        uprint!($serial, concat!($fmt, "\n"), $($arg)*)
-    };
-}
-
-struct SerialPort {
-    usart1: &'static mut usart1::RegisterBlock,
-}
-
-impl fmt::Write for SerialPort {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        for byte in s.bytes() {
-            // wait until it's safe to write to TDR
-            while self.usart1.isr.read().txe().bit_is_clear() {}
-            self.usart1.tdr.write(|w| w.tdr().bits(u16::from(byte)));
-        }
-
-        Ok(())
-    }
-}
+use aux11::{entry, iprint, iprintln};
+use heapless::{consts, Vec};
 
 #[entry]
 fn main() -> ! {
     let (usart1, mono_timer, itm) = aux11::init();
 
-    // let mut serial = SerialPort { usart1 };
-
-    // uprintln!(serial, "The answer is {}", 40 + 2);
+    // A buffer with 32 bytes of capacity
+    let mut buffer: Vec<u8, consts::U32> = Vec::new();
 
     loop {
-        // Wait until there's data available
-        while usart1.isr.read().rxne().bit_is_clear() {}
+        buffer.clear();
+        // Receive a user request. Each user request ends with ENTER
+        // NOTE `buffer.push` returns a `Result`. Handle the error by responding
+        // with an error message.
+        loop {
+            // wait until there's data available
+            while usart1.isr.read().rxne().bit_is_clear() {}
 
-        // Retrieve the data
-        let byte = usart1.rdr.read().rdr().bits() as u8;
+            // retrieve the data
+            let byte = usart1.rdr.read().rdr().bits() as u8;
 
-        // Wait until it's safe to write to TDR and send data
-        while usart1.isr.read().txe().bit_is_clear() {}
-        usart1.tdr.write(|w| w.tdr().bits(byte.into()));
+            if buffer.push(byte).is_err() {
+                for c in b"Error: buffer is full\n\r".iter() {
+                    while usart1.isr.read().txe().bit_is_clear() {}
+                    usart1.tdr.write(|w| w.tdr().bits(u16::from(*c)));
+                }
+                break;
+            }
+
+            // Enter pressed
+            if byte == 13 {
+                // Send back the reversed string
+                usart1.tdr.write(|w| w.tdr().bits(u16::from(b'\n')));
+                usart1.tdr.write(|w| w.tdr().bits(u16::from(b'\r')));
+                while let Some(c) = buffer.pop() {
+                    usart1.tdr.write(|w| w.tdr().bits(u16::from(c)));
+                }
+
+                break;
+            }
+        }
     }
 }

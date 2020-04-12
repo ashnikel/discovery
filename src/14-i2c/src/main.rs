8379edaab76c80b24177f1aee7b3cf8adb9238cc
ashnikel <ashnikel@gmail.com>
2020-04-12 17:31:14 +0300

    Read several registers

--- src/14-i2c/src/main.rs
+++ src/14-i2c/src/main.rs
@@ -14,11 +14,11 @@ const IRA_REG_M: u8 = 0x0A;
 
 #[entry]
 fn main() -> ! {
-    let (i2c1, _delay, mut itm) = aux14::init();
+    let (i2c1, mut delay, mut itm) = aux14::init();
 
     // Stage 1: Send the address of the register we want to read to the
     // magnetometer
-    {
+    loop {
         // Broadcast START
         // Broadcast the MAGNETOMETER address with the R/W bit set to Write
         i2c1.cr2.write(|w| {
@@ -32,34 +32,45 @@ fn main() -> ! {
         // Wait until we can send more data
         while i2c1.isr.read().txis().bit_is_clear() {}
 
-        // Send the address of the register that we want to read: IRA_REG_M
-        i2c1.txdr.write(|w| w.txdata().bits(IRA_REG_M));
+        // Send the address of the register that we want to read: OUT_X_H_M
+        i2c1.txdr.write(|w| w.txdata().bits(OUT_X_H_M));
 
         // Wait until the previous byte has been transmitted
         while i2c1.isr.read().tc().bit_is_clear() {}
-    }
 
-    // Stage 2: Receive the contents of the register we asked for
-    let byte = {
+        // Stage 2: Receive the contents of the register we asked for
         // Broadcast RESTART
         // Broadcast the MAGNETOMETER address with the R/W bit set to Read
         i2c1.cr2.modify(|_, w| {
             w.start().set_bit();
-            w.nbytes().bits(1);
+            w.nbytes().bits(6);
             w.rd_wrn().set_bit();
             w.autoend().set_bit()
         });
 
-        // Wait until we have received the contents of the register
-        while i2c1.isr.read().rxne().bit_is_clear() {}
+        let mut buffer = [0u8; 6];
+        for byte in &mut buffer {
+            // Wait until we have received the contents of the register
+            while i2c1.isr.read().rxne().bit_is_clear() {}
+
+            *byte = i2c1.rxdr.read().rxdata().bits();
+        }
 
         // Broadcast STOP (automatic because of `AUTOEND = 1`)
 
-        i2c1.rxdr.read().rxdata().bits()
-    };
+        let x_h = u16::from(buffer[0]);
+        let x_l = u16::from(buffer[1]);
+        let z_h = u16::from(buffer[2]);
+        let z_l = u16::from(buffer[3]);
+        let y_h = u16::from(buffer[4]);
+        let y_l = u16::from(buffer[5]);
 
-    // Expected output: 0x0A - 0b01001000
-    iprintln!(&mut itm.stim[0], "0x{:02X} - 0b{:08b}", IRA_REG_M, byte);
+        let x = ((x_h << 8) + x_l) as i16;
+        let y = ((y_h << 8) + y_l) as i16;
+        let z = ((z_h << 8) +z_l) as i16;
 
-    loop {}
+        iprintln!(&mut itm.stim[0], "{:?}", (x, y, z));
+
+        delay.delay_ms(1_000_u16);
+    }
 }

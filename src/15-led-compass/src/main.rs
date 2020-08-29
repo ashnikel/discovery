196c95b9d675e2876752a24c1a1b75323d1ffb97
ashnikel <ashnikel@gmail.com>
2020-08-29 20:40:19 +0300

    LED compass Take 1

--- src/15-led-compass/src/main.rs
+++ src/15-led-compass/src/main.rs
@@ -3,14 +3,29 @@
 #![no_std]
 
 #[allow(unused_imports)]
-use aux15::{entry, iprint, iprintln, prelude::*};
+use aux15::{entry, iprint, iprintln, prelude::*, Direction, I16x3};
 
 #[entry]
 fn main() -> ! {
-    let (_leds, mut lsm303dlhc, mut delay, mut itm) = aux15::init();
-
+    let (mut leds, mut lsm303dlhc, mut delay, mut _itm) = aux15::init();
     loop {
-        iprintln!(&mut itm.stim[0], "{:?}", lsm303dlhc.mag().unwrap());
+        let I16x3 { x, y, .. } = lsm303dlhc.mag().unwrap();
+        // Look at the signs of the X and Y components to determine in which quadrant
+        // the magnetic field is
+        let dir = match (x > 0, y > 0) {
+            // Quadrant I
+            (true, true) => Direction::Southeast,
+            // Quadrant IV
+            (true, false) => Direction::Southwest,
+            // Quadrant III
+            (false, false) => Direction::Northwest,
+            // Quadrant II
+            (false, true) => Direction::Southeast,
+        };
+
+        leds.iter_mut().for_each(|led| led.off());
+        leds[dir].on();
+
         delay.delay_ms(1_000_u16);
     }
 }

b281ce6d52deab9f1c5d385f18e273915a32144c
ashnikel <ashnikel@gmail.com>
2020-08-30 20:05:52 +0300

    LED compass Take 2

--- src/15-led-compass/src/main.rs
+++ src/15-led-compass/src/main.rs
@@ -2,30 +2,49 @@
 #![no_main]
 #![no_std]
 
+use core::f32::consts::PI;
+
 #[allow(unused_imports)]
 use aux15::{entry, iprint, iprintln, prelude::*, Direction, I16x3};
+
+// this trait provides the `atan2` method
+use m::Float;
 
 #[entry]
 fn main() -> ! {
     let (mut leds, mut lsm303dlhc, mut delay, mut _itm) = aux15::init();
+
     loop {
         let I16x3 { x, y, .. } = lsm303dlhc.mag().unwrap();
-        // Look at the signs of the X and Y components to determine in which quadrant
-        // the magnetic field is
-        let dir = match (x > 0, y > 0) {
-            // Quadrant I
-            (true, true) => Direction::Southeast,
-            // Quadrant IV
-            (true, false) => Direction::Southwest,
-            // Quadrant III
-            (false, false) => Direction::Northwest,
-            // Quadrant II
-            (false, true) => Direction::Southeast,
+
+        let theta = (y as f32).atan2(x as f32);
+
+        // FIXME pick a direction to point to based on `theta`
+        let dir = {
+            if theta < -7.0 * PI / 8.0 {
+                Direction::North
+            } else if theta < -5.0 * PI / 8.0 {
+                Direction::Northwest
+            } else if theta < -3.0 * PI / 8.0 {
+                Direction::West
+            } else if theta < -PI / 8.0 {
+                Direction::Southwest
+            } else if theta < PI / 8.0 {
+                Direction::South
+            } else if theta < 3.0 * PI / 8.0 {
+                Direction::Southeast
+            } else if theta < 5.0 * PI / 8.0 {
+                Direction::East
+            } else if theta < 7.0 * PI / 8.0 {
+                Direction::Northeast
+            } else {
+                Direction::North
+            }
         };
 
         leds.iter_mut().for_each(|led| led.off());
         leds[dir].on();
 
-        delay.delay_ms(1_000_u16);
+        delay.delay_ms(100_u8);
     }
 }

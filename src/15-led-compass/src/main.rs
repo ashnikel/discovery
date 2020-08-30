7898c27fb5f5607bd7a0c24a2e8b5e52f5a76e6d
ashnikel <ashnikel@gmail.com>
2020-08-30 20:25:53 +0300

    LED compass Magnitude

--- src/15-led-compass/src/main.rs
+++ src/15-led-compass/src/main.rs
@@ -12,39 +12,21 @@ use m::Float;
 
 #[entry]
 fn main() -> ! {
-    let (mut leds, mut lsm303dlhc, mut delay, mut _itm) = aux15::init();
+    const XY_GAIN: f32 = 1100.; // LSB / G
+    const Z_GAIN: f32 = 980.; // LSB / G
+
+    let (mut _leds, mut lsm303dlhc, mut delay, mut itm) = aux15::init();
 
     loop {
-        let I16x3 { x, y, .. } = lsm303dlhc.mag().unwrap();
+        let I16x3 { x, y, z } = lsm303dlhc.mag().unwrap();
 
-        let theta = (y as f32).atan2(x as f32);
+        let x = f32::from(x) / XY_GAIN;
+        let y = f32::from(y) / XY_GAIN;
+        let z = f32::from(z) / Z_GAIN;
 
-        // FIXME pick a direction to point to based on `theta`
-        let dir = {
-            if theta < -7.0 * PI / 8.0 {
-                Direction::North
-            } else if theta < -5.0 * PI / 8.0 {
-                Direction::Northwest
-            } else if theta < -3.0 * PI / 8.0 {
-                Direction::West
-            } else if theta < -PI / 8.0 {
-                Direction::Southwest
-            } else if theta < PI / 8.0 {
-                Direction::South
-            } else if theta < 3.0 * PI / 8.0 {
-                Direction::Southeast
-            } else if theta < 5.0 * PI / 8.0 {
-                Direction::East
-            } else if theta < 7.0 * PI / 8.0 {
-                Direction::Northeast
-            } else {
-                Direction::North
-            }
-        };
+        let mag = (x * x + y * y + z * z).sqrt();
 
-        leds.iter_mut().for_each(|led| led.off());
-        leds[dir].on();
-
-        delay.delay_ms(100_u8);
+        iprintln!(&mut itm.stim[0], "{} mG", mag * 1_000.);
+        delay.delay_ms(500_u16);
     }
 }
